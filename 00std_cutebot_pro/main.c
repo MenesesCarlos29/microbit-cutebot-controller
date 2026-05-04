d17755f9259145e959ce15f79b432b25f4f983a1
MenesesCarlos29 <carlitosmengam.29@gmail.com>
2026-05-04 09:34:18 +0200

    Project creation

---
+++ 00std_cutebot_pro/main.c
@@ -0,0 +1,6 @@
+#include "modules/ex2_full_speed.h"
+
+int main(void) {
+    run_full_speed();
+    while(1);
+}
---
+++ 00std_cutebot_pro/modules/ex2_full_speed.h
@@ -0,0 +1,6 @@
+#ifndef EX2_FULL_SPEED_H
+#define EX2_FULL_SPEED_H
+
+void run_full_speed();
+
+#endif // EX2_FULL_SPEED_H
